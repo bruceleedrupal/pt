82d84a3e93a9575ce54ace60750ab95b95f863b8
brucelee <brucelee.drupal@gmail.com>
2020-05-14 17:10:14 +0800

    save

--- src/Controller/WechatController.php
+++ src/Controller/WechatController.php
@@ -30,32 +30,66 @@ class WechatController extends AbstractController
            'aes_key'  => 'Cc54S58C1k40NMunZujJMs2fx6M6J2K66q6QJCK81MF'
         ];
 
+    private $mobileDetect;
+    
     public function __construct() {
-	$this->openPlatform= Factory::openPlatform($this->config); 
+    	$this->openPlatform= Factory::openPlatform($this->config); 
+    	$this->mobileDetect = new \Mobile_Detect;
 	}
 
 
-
+	public function authUrlBlock(){
+	    $callbackUrl = 'https://pt.iixiao.cn/wechat/handleAuthorize';
+	    
+	    $isMobile = $this->mobileDetect->isMobile();
+	    if($isMobile)
+	        $authUrl = $this->openPlatform->getMobilePreAuthorizationUrl($callbackUrl); // 传入回调URI即可
+	    else 
+	        $authUrl = $this->openPlatform->getPreAuthorizationUrl($callbackUrl); // 传入回调URI即可	
+        return $this->render('wechat/authUrlBlock.html.twig', [
+            'authUrl' => $authUrl            
+        ]);
+	        
+	          
+	}
+	
+	
     /**
      * @Route("/", name="wechat")
      */
     public function wechat()
     {
 
-    $callbackUrl = 'https://pt.iixiao.cn/wechat/handleAuthorize';
-	$url = $this->openPlatform->getPreAuthorizationUrl($callbackUrl); // 传入回调URI即可	
-	$url2 = $this->openPlatform->getMobilePreAuthorizationUrl($callbackUrl); // 传入回调URI即可	
+        $callbackUrl = 'https://pt.iixiao.cn/wechat/handleAuthorize';
+    	$url = $this->openPlatform->getPreAuthorizationUrl($callbackUrl); // 传入回调URI即可	
+    	$url2 = $this->openPlatform->getMobilePreAuthorizationUrl($callbackUrl); // 传入回调URI即可	
 
-	//$authorizer = $this->openPlatform->getAuthorizer('wx0be3bba47580b889');
+    // $result=     $authorizer = $this->openPlatform->getAuthorizer('wx0be3bba47580b889');
     
       
-	  $app = $this->openPlatform->officialAccount('wx0be3bba47580b889', 'refreshtoken@@@Uwk_BGhkeiXwpkARIgkoayfTZfgCjeuBo6HuTQpxvbE');
-
-    
-	  $result = $app->template_message->getPrivateTemplates();
-    
+       //2020.05.08 16.04
+    //  $app = $this->openPlatform->officialAccount('wx0be3bba47580b889', 'refreshtoken@@@riwDd3Bl-dFJE9_a8sWT44oyVoP0jXdE1x4bmeSSujc');
   
     
+     
+	 
+	  
+	//  $result = $app->user->list();
+	  /*
+	 $result =  $app->template_message->send([
+        'touser' => 'oqYJP1c4pGo2Nl9dpTrZ0hZmfcoE',
+        'template_id' => 'vEKbY7jV9U5D0uyQ6Sifm5zzatqbN8H_uVZ3Ddn0gmk',
+        'data' => [
+            'first' => 'VALUE',
+            'keyword1' => 'VALUE1',
+            'keyword2' => '<a href="tel:18116381898">18116381898</a>',
+            'keyword3' => 'VALUE3',
+            'remark' => 'VALUE3',
+        ],
+    ]);
+    
+  
+    */
     
 
     
@@ -64,7 +98,8 @@ class WechatController extends AbstractController
         return $this->render('wechat/index.html.twig', [
             'url' => $url,
             'url2'=>$url2,
-            'result'=>$result,          
+         //   'result'=>$result,          
+	    //'authorizer'=>$authorizer,
             
         ]);
     }
